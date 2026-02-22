4aa7c945ac58aac648bd1b9d3298f00460b44949
jonathanabehr-collab <jonathanabehr@gmail.com>
2026-02-21 20:45:20 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import RenewalPipeline from './App';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RenewalPipeline />
+  </React.StrictMode>
+);
